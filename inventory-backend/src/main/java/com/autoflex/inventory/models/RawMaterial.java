e5c637fb721b1176d065c4acdeb3c27318342b65
Leticia Bezerra <leticiabezerrauy@gmail.com>
2026-03-02 23:22:15 -0300

    feat: setup react frontend with tailwind and raw materials crud

--- inventory-backend/src/main/java/com/autoflex/inventory/models/RawMaterial.java
+++ inventory-backend/src/main/java/com/autoflex/inventory/models/RawMaterial.java
@@ -1,4 +1,3 @@
-
 package com.autoflex.inventory.models;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
@@ -7,5 +6,5 @@ import jakarta.persistence.Entity;
 @Entity
 public class RawMaterial extends PanacheEntity {
     public String name;
-    public double quantity; 
+    public Double quantity;
 }
